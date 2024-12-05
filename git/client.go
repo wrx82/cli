DevSauce-Official/Promo-Sauce-CLI

Find push remote using branch.<name>.pushRemote and remote.pushDefault
When using a push.default = current triangular workflow, apart from
using @{push} to determine the remote branch name, we should also follow
the

1. branch.<name>.pushRemote
2. remote.pushDefault
3. branch.<name>.remote

...list to determine which remote Git pushes to.

git/client.go
@@ -325,7 +325,7 @@ func (c *Client) lookupCommit(ctx context.Context, sha, format string) ([]byte, 
 // ReadBranchConfig parses the `branch.BRANCH.(remote|merge)` part of git config.
 func (c *Client) ReadBranchConfig(ctx context.Context, branch string) (cfg BranchConfig) {
 	prefix := regexp.QuoteMeta(fmt.Sprintf("branch.%s.", branch))
-	args := []string{"config", "--get-regexp", fmt.Sprintf("^%s(remote|merge)$", prefix)}
+	args := []string{"config", "--get-regexp", fmt.Sprintf("^%s(remote|merge|pushremote)$", prefix)}
 	cmd, err := c.Command(ctx, args...)
 	if err != nil {
 		return
@@ -342,17 +342,18 @@ func (c *Client) ReadBranchConfig(ctx context.Context, branch string) (cfg Branc
 		keys := strings.Split(parts[0], ".")
 		switch keys[len(keys)-1] {
 		case "remote":
-			if strings.Contains(parts[1], ":") {
-				u, err := ParseURL(parts[1])
-				if err != nil {
-					continue
-				}
-				cfg.RemoteURL = u
-			} else if !isFilesystemPath(parts[1]) {
-				cfg.RemoteName = parts[1]
-			}
+			parseRemoteURLOrname(parts[1], &cfg.RemoteURL, &cfg.RemoteName)
+		case "pushremote":
+			parseRemoteURLOrname(parts[1], &cfg.PushRemoteURL, &cfg.PushRemoteName)
 		case "merge":
 			cfg.MergeRef = parts[1]
+		}
+	}
+	if cfg.PushRemoteURL == nil && cfg.PushRemoteName == "" {
+		if conf, err := c.Config(ctx, "remote.pushDefault"); err == nil && conf != "" {
+			parseRemoteURLOrname(conf, &cfg.PushRemoteURL, &cfg.PushRemoteName)
+		} else {
+			cfg.PushRemoteName = cfg.RemoteName
 		}
 	}
 	if out, err = c.revParse(ctx, "--verify", "--quiet", "--abbrev-ref", branch+"@{push}"); err == nil {
@@ -693,6 +694,16 @@ func parseRemotes(remotesStr []string) RemoteSet {
 		}
 	}
 	return remotes
+}
+
+func parseRemoteURLOrname(value string, remoteURL **url.URL, remoteName *string) {
+	if strings.Contains(value, ":") {
+		if u, err := ParseURL(value); err == nil {
+			*remoteURL = u
+		}
+	} else if !isFilesystemPath(value) {
+		*remoteName = value
+	}
 }
 
 func populateResolvedRemotes(remotes RemoteSet, resolved []string) {

wrx82/cli

git/client.go
@@ -376,7 +376,7 @@ func (c *Client) lookupCommit(ctx context.Context, sha, format string) ([]byte, 
 // ReadBranchConfig parses the `branch.BRANCH.(remote|merge)` part of git config.
 func (c *Client) ReadBranchConfig(ctx context.Context, branch string) (cfg BranchConfig) {
 	prefix := regexp.QuoteMeta(fmt.Sprintf("branch.%s.", branch))
-	args := []string{"config", "--get-regexp", fmt.Sprintf("^%s(remote|merge)$", prefix)}
+	args := []string{"config", "--get-regexp", fmt.Sprintf("^%s(remote|merge|pushremote)$", prefix)}
 	cmd, err := c.Command(ctx, args...)
 	if err != nil {
 		return
@@ -393,17 +393,18 @@ func (c *Client) ReadBranchConfig(ctx context.Context, branch string) (cfg Branc
 		keys := strings.Split(parts[0], ".")
 		switch keys[len(keys)-1] {
 		case "remote":
-			if strings.Contains(parts[1], ":") {
-				u, err := ParseURL(parts[1])
-				if err != nil {
-					continue
-				}
-				cfg.RemoteURL = u
-			} else if !isFilesystemPath(parts[1]) {
-				cfg.RemoteName = parts[1]
-			}
+			parseRemoteURLOrName(parts[1], &cfg.RemoteURL, &cfg.RemoteName)
+		case "pushremote":
+			parseRemoteURLOrName(parts[1], &cfg.PushRemoteURL, &cfg.PushRemoteName)
 		case "merge":
 			cfg.MergeRef = parts[1]
+		}
+	}
+	if cfg.PushRemoteURL == nil && cfg.PushRemoteName == "" {
+		if conf, err := c.Config(ctx, "remote.pushDefault"); err == nil && conf != "" {
+			parseRemoteURLOrName(conf, &cfg.PushRemoteURL, &cfg.PushRemoteName)
+		} else {
+			cfg.PushRemoteName = cfg.RemoteName
 		}
 	}
 	if out, err = c.revParse(ctx, "--verify", "--quiet", "--abbrev-ref", branch+"@{push}"); err == nil {
@@ -755,6 +756,16 @@ func parseRemotes(remotesStr []string) RemoteSet {
 		}
 	}
 	return remotes
+}
+
+func parseRemoteURLOrName(value string, remoteURL **url.URL, remoteName *string) {
+	if strings.Contains(value, ":") {
+		if u, err := ParseURL(value); err == nil {
+			*remoteURL = u
+		}
+	} else if !isFilesystemPath(value) {
+		*remoteName = value
+	}
 }
 
 func populateResolvedRemotes(remotes RemoteSet, resolved []string) {